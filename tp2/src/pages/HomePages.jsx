49f54a84ef59388eab4a8afc3199dee9af9c2370
Rodrigo <met.sistemas@gmail.com>
2025-04-26 13:37:07 -0300

    Agrego un subtítulo en HomePages.jsx

--- tp2/src/pages/HomePages.jsx
+++ tp2/src/pages/HomePages.jsx
@@ -4,6 +4,8 @@ function HomePages() {
   return (
     <main>
       <h2>Bienvenidos a nuestra página grupal</h2>
+      <h3>Trabajo Práctico 2 - Programación III</h3>
+
       <p>En esta web se presenta a cada integrante del grupo de Programación 3.</p>
     </main>
   );
